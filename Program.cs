df842028bd3ed5215e010541dcf7f90f3883da8e
Waldo Heudens <waldo@heudens.be>
2021-10-06 11:14:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Gedrag van "Value Type" variabele
+
+using TypesInCS;
+
+int a = 15;
+int b = a;
+a = 30;
+
+Console.WriteLine("a = " + a + "    b = " + b);
+
+// Gedrag van "Reference Type" variabele
+
+TestClass tcA = new TestClass(15, "Eerste Object");
+TestClass tcB = new TestClass(45, "Derde Object");
+tcB = tcA;
+tcA.Id = 30;
+tcA.Name = "Tweede Object";
+
+Console.WriteLine("tcA= " + tcA + "    tcB= " + tcB);
+// Zowel tcA als tcB zijn gewijzigd.
---
+++ TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesInCS
+{
+    internal class TestClass
+    {
+        public int Id {  get; set; }
+        public string Name {  get; set; }  
+
+        public TestClass()
+        {
+            // Als goede praktijk:  Voorzie altijd een default-constructor, als ook een andere constructor aanwezig is
+        }
+
+        public TestClass (int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + ", Name:" + Name;
+        }
+    }
+}
